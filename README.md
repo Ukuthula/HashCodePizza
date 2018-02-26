24833ec6580e534b2c527b748a1df098f66d38d4
Ukuthula <julian.hesse4@gmx.de>
2018-02-27 00:30:17 +0100

    added all files needed

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # HashCodePizza
 Trying to solve the example exercise given by Google.
+
+Start program:
+
+'''$ python main.py  inputfile.in outputfile.in '''
---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+import math
+import numpy as np
+
+
+def main():
+    '''
+    Main Function
+    '''
+
+    # First the input has to be converted into an array
+
+    # Second the pizza has to be sliced up in pieces that fullfil the requirements
+
+    # Third the slices have to be written to an outputfile
+
