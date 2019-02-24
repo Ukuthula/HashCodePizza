9aa6e5014a84f31fd6704b614c94e98796976c60
numberking123 <clemens.mosig@gmail.com>
2019-02-24 20:03:38 +0100

    adds run method

--- pizza.py
+++ pizza.py
@@ -3,24 +3,19 @@ import numpy as np
 import pandas as pd
 
 #%%
-example_param = pd.read_csv('data/example.in',sep=' ', header=None, nrows=1)
-example = pd.read_csv('data/example.in',sep=' ', header=None, skiprows=1)
-example = example[0].apply(lambda x: pd.Series(list(x)))
-example[example == 'T'] = 0
-example[example == 'M'] = 1
-example = np.array(example, dtype=int)
-exR = int(example_param[0])
-exC = int(example_param[1])
-exL = int(example_param[2])
-exH = int(example_param[3])
-slices = []
 
+def run( path_in, path_out ):
+    data_param = pd.read_csv(path, sep=' ', header=None, nrows=1)
+    data = pd.read_csv(path,sep=' ', header=None, skiprows=1)
+    data = data[0].apply(lambda x: pd.Series(list(x)))
+    data[data == 'T'] = 0
+    data[data == 'M'] = 1
+    data = np.array(data, dtype=int)
+    dataR = int(data_param[0])
+    dataC = int(data_param[1])
+    dataL = int(data_param[2])
+    dataH = int(data_param[3])
 
-#%%
-print(exR, exC, exL, exH)
-
-#%%
-int(np.sqrt(exH))
 
 #%%
 def generateSlices(R,C,L,H):
